a33484f83e94fe4f749617a93f5c68abe4c318f9
HannahAlAssad <hannahalassad@gmail.com>
2025-05-12 20:08:08 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const suites = ['♦', '♥', '♠', '♣'];
+  const numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+
+
+
+  const getRandomElements = (array) => {
+    const randomElements = Math.floor(Math.random() * array.length)
+    return array[randomElements] 
+  }
+  const suiteValue = getRandomElements(suites)
+  const numberValue = getRandomElements(numbers)
+
+  let number = document.querySelector("#number")
+  let top = document.querySelector("#top")
+  let bottom = document.querySelector("#bottom")
+  number.innerHTML = numberValue
+  top.innerHTML = suiteValue
+  bottom.innerHTML = suiteValue
+
+  top.classList.add(suiteValue)
+  bottom.classList.add(suiteValue)
 };
